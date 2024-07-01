77088f0e9195d18bc563aab7d89885afb477bb48
Caio <caioagralemos@live.com>
2024-07-01 20:18:23 -0300

    Initial Commit

---
+++ Direction/DirectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectionApp.swift
+//  Direction
+//
+//  Created by Caio on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DirectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
